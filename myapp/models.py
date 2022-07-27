402c93f5c32e2bc9a9b521ac0e2ef9fb9cae9622
Brian Nguyen <nguyenbriankhoi@gmail.com>
2022-07-27 01:20:09 -0700

    amazon product dataset loaded

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,22 @@
+
 from django.db import models
 
+
 # Create your models here.
+
+class Products(models.Model):
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.CharField(max_length=200, null=True)
+    ratings = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True)
+    url = models.CharField(max_length=200, null=True)
+    imageSrc = models.CharField(max_length=200, null=True)
+
+
+
+    def __str__(self):  
+        return self.name
+
+
+  
