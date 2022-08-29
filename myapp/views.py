64e44269cf9bfc75d9ba9155944a2e203a28e8be
Harjot Singh <harjot.singh0712@gmail.com>
2022-08-29 01:37:55 -0700

    Finish revealHints() javascript method using AJAX to get db info

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,14 @@
+from django.http import JsonResponse
+from django.core import serializers
 from django.shortcuts import render
 from .models import *
 from json import dumps
+from json import loads
 
 # Create your views here.
 def index(request):
-    result = Products.objects.raw('SELECT *, price AS price FROM myapp_products ORDER BY RANDOM() LIMIT 1')[0]
+    result = Products.objects.raw('SELECT * FROM myapp_products ORDER BY RANDOM() LIMIT 1')[0]
+    id = dumps("%s" % (result.id))
     name = dumps("%s" % (result.name))
     price = dumps("%s" % (result.price[1:]))
     ratings = dumps("%s" % (result.ratings))
@@ -12,4 +16,22 @@ def index(request):
     url = dumps("%s" % (result.url))
     imageSrc = "%s" % (result.imageSrc)
 
-    return render(request, "myapp/home.html", context={"result": result, "name": name, "price": price, "ratings": ratings, "category": category, "url": url, "imageSrc": imageSrc})
+    return render(request, "myapp/home.html", context={"id": id, "result": result, "name": name, "price": price, "ratings": ratings, "category": category, "url": url, "imageSrc": imageSrc})
+
+
+
+def getDBData(request):
+    # Use request id to get relevant data from db
+    id = loads(request.body.decode('utf-8'))['id']
+    result = Products.objects.raw('SELECT * FROM myapp_products WHERE id=%s', [id])[0]
+    name = dumps("%s" % (result.name))
+    price = dumps("%s" % (result.price[1:]))
+    ratings = dumps("%s" % (result.ratings))
+    category = dumps("%s" % (result.category))
+    url = dumps("%s" % (result.url))
+
+    # send a json response 
+    return JsonResponse({ 'name':name, 'price':price, 'ratings':ratings, 'category':category, 'url': url }, status=200)
+
+    # some error occured
+    return JsonResponse({"error": ""}, status=400)
--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,16 @@ document.getElementById("hint").addEventListener("click", revealHint);
 var totalScore = document.getElementById('totalScore').value;
 var productScore = document.getElementById('totalScore').value;
 
+function getCookieValue(name) 
+{
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) {
+        return match[2];
+    }
+    else {
+        console.log('--something went wrong---');
+    }
+}
 
 function calculateScore() {
     var score = 0;
@@ -12,36 +22,46 @@ function calculateScore() {
     return score;
 }
 
-function productEstimate() {
-    return (Math.random() * price).toFixed(2) + " to " + ((Math.random() + 1) * price).toFixed(2);
+function productEstimate(price) {
+    return "$" + (Math.random() * price).toFixed(2) + " to $" + ((Math.random() + 1) * price).toFixed(2);
 }
 
-var revealHint = (function() {
+var revealHint = (function(prodId) {
     var hints = 0;
-    // var title = JSON.parse("{{name|escapejs}}");
-    // var price = JSON.parse("{{price|escapejs}}");
-    // var ratings = JSON.parse("{{ratings|escapejs}}");
-    // var category = JSON.parse("{{category|escapejs}}");
-    // var url = JSON.parse("{{url|escapejs}}");
-    return function() {
-        if (hints == 0) {
-            // hint1.innerHTML = "Product Ratings " + ratings;
-            hint1.style.display = "block"
-            modifier = 0.95;
-        } else if (hints == 1) {
-            // hint2.innerHTML = "Product Name: " + title;
-            hint2.style.display = "block"
-            modifier = 0.85;
-        } else if (hints == 2) {
-            // hint3.innerHTML = "Product Price Range " + productEstimate();
-            hint3.style.display = "block"
-            modifer = 0.65;
+    return function(prodId) {
+        // Create an XMLHttpRequest object
+        const xhttp = new XMLHttpRequest();
+
+        // Define a callback function
+        xhttp.onload = function() {
+            var response = JSON.parse(xhttp.responseText);
+            var name = response.name;
+            var ratings = response.ratings;
+            var category = response.category;
+            var price = response.price.slice(1,-1);
+            var url = response.url;
+
+            if (hints == 0) {
+                hint1.innerHTML = "Product Ratings: " + ratings;
+                hint1.style.display = "block"
+                modifier = 0.95;
+            } else if (hints == 1) {
+                hint2.innerHTML = "Product Name: " + name;
+                hint2.style.display = "block"
+                modifier = 0.85;
+            } else if (hints == 2) {
+                hint3.innerHTML = "Product Price Range " + productEstimate(parseFloat(price));
+                hint3.style.display = "block"
+                modifer = 0.65;
+            }
+            if(hints < 3) {
+                hints++;
+            }
         }
-        if(hints < 3) {
-            hints++;
-        }
+
+        // Send a request
+        xhttp.open("POST", "getDBData");
+        xhttp.setRequestHeader("X-CSRFToken", getCookieValue("csrftoken"));
+        xhttp.send(JSON.stringify({id: prodId}));
     };
 })();
-
-function revealHint() {
-}
